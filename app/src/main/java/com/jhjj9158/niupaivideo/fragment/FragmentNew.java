776c04a0bb29079dc41365e2dec71215c89d6791
kiddo oldman <kiddo oldman>
2017-04-28 08:47:05 +0800

    fix

--- app/src/main/java/com/jhjj9158/niupaivideo/fragment/FragmentNew.java
+++ app/src/main/java/com/jhjj9158/niupaivideo/fragment/FragmentNew.java
@@ -25,6 +25,7 @@ import com.jhjj9158.niupaivideo.bean.IndexBean;
 import com.jhjj9158.niupaivideo.utils.AESUtil;
 import com.jhjj9158.niupaivideo.utils.Contact;
 import com.jhjj9158.niupaivideo.widget.GridSpacingItemDecoration;
+import com.jhjj9158.niupaivideo.widget.SpaceItemDecoration;
 
 import java.io.IOException;
 import java.util.List;
@@ -97,7 +98,8 @@ public class FragmentNew extends Fragment {
                 StaggeredGridLayoutManager.VERTICAL);
         recyclerview.setLayoutManager(layoutManager);
 
-        recyclerview.addItemDecoration(new GridSpacingItemDecoration(2,10,true));
+//      recyclerview.addItemDecoration(new GridSpacingItemDecoration(2,10,true));
+        recyclerview.addItemDecoration(new SpaceItemDecoration(5));
         recyclerview.setItemAnimator(new DefaultItemAnimator());
         recyclerview.setHasFixedSize(true);
 
